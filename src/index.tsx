feaf8ccfbea74588873d61114b58d90832a143ba
Sergey Sova <s.sova@redmadrobot.com>
2020-05-05 19:14:32 +0300

    feat: add generic template

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
+import 'antd/dist/antd.min.css';
 import './index.css';
 import { Application } from './application';
 
--- src/ui/templates/generic.tsx
+++ src/ui/templates/generic.tsx
@@ -1,13 +1,21 @@
 import * as React from 'react';
 import './generic.css';
 import { Link } from 'react-router-dom';
+import { Layout, Menu } from 'antd';
+
 import { paths } from 'pages/paths';
 
 export const GenericTemplate: React.FC = ({ children }) => (
-  <>
-    <nav className="navigation">
-      <Link to={paths.home()}>Home</Link>
-    </nav>
-    <main>{children}</main>
-  </>
+  <Layout>
+    <Layout.Header>
+      <Menu theme="dark" mode="horizontal">
+        <Menu.Item>
+          <Link to={paths.home()}>Home</Link>
+        </Menu.Item>
+      </Menu>
+    </Layout.Header>
+    <Layout.Content style={{ justifyContent: 'center' }}>
+      {children}
+    </Layout.Content>
+  </Layout>
 );
