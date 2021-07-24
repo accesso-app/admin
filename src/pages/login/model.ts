8438fb3fb8aaa435a01bad670a8f413bb2c031b5
Dmitriy Chikhanov <bosshet@gmail.com>
2021-07-24 15:38:21 +0300

    feat: handle login button click

--- src/pages/login/model.ts
+++ src/pages/login/model.ts
@@ -1,3 +1,19 @@
-import { createEvent } from 'effector';
+import { createEffect, createEvent, forward } from 'effector';
 
-export const loginClick = createEvent();
+export const loginClicked = createEvent();
+
+export const handleAuthResponse = createEffect();
+
+forward({
+  from: loginClicked,
+  to: loadAuthSettingsFx,
+});
+
+loadAuthSettingsFx.done.watch(({ result }) => {
+  window.location.href = generateOAuthPath(result);
+});
+
+function generateOAuthPath(settings: OAuthSettings): string {
+  const authServerUrl = 'https://localhost:3000/login';
+  return `${authServerUrl}?client_id=${settings.clientId}&redirect_uri=${settings.redirectUri}&response_type=code`;
+}
