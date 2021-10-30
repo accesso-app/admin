e5492c7c103c106bdc106bf8c53bbe2316ed1353
Sergey Sova <mail@sergeysova.com>
2021-10-30 14:35:25 +0300

    fix: do not babel node modules

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,4 @@
 module.exports = {
+  presets: ['patronum/babel-preset'],
   plugins: ['effector/babel-plugin'],
 };
--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,12 @@
 import path from 'path';
 import { defineConfig } from 'vite';
 
+import babel from '@rollup/plugin-babel';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        babelrc: true,
-        configFile: true,
-      },
-    }),
-  ],
+  plugins: [react(), babel({ extensions: ['.ts', '.tsx'] })],
   resolve: {
     alias: [{ find: '~', replacement: path.resolve('src') }],
   },
